4563835062b9a2ddac2f3ec6ca44da86553b4aef
youngseon098-blip <youngseon098-blip@users.noreply.github.com>
2026-04-24 08:55:18 +0000

    deploy: 9edd482bcf8554e995d01e1349dba57e803d76f8

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Office-of-Education-program/sw.js', { scope: '/Office-of-Education-program/' })})}
